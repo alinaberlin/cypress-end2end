451bf54738e17503d34c18ba126c6c27cc107904
Alina Ghetler <alina.ghetler@gmail.com>
2020-09-29 14:32:30 +0200

    Add negative test for login with fancied email

--- frontend/cypress/integration/login.spec.js
+++ frontend/cypress/integration/login.spec.js
@@ -8,14 +8,21 @@ it('should navigate to the login page', () => {
     cy.get('[type=submit]').click()
 
 })
-//negative test wrong password for existing user
+//negative test with wrong password for existing user
 it('should navigate to the login page', () => {
-
     cy.visit('http://picories.localhost/login')
     cy.get('input[type=email]').type('a@gmail.com')
-
     cy.get('input[type=password]').type('whatever123')
-
     cy.get('[type=submit]').click()
+    cy.contains('Register').click()
 
+})
+//negative test for login with false email
+
+it('should navigate to the login page', () => {
+    cy.visit('http://picories.localhost/login')
+    cy.get('input[type=email]').type('2@gmail.com')
+    cy.get('input[type=password]').type('whatever123')
+    cy.get('[type=submit]').click()
+    cy.contains('Register').click()
 })
